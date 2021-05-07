c46f5202e9d420656e9da41b0fb48208ec87bc5a
Mingxun Wang <mwang87@gmail.com>
2021-05-07 11:27:33 -0700

    refactor and api

--- app.py
+++ app.py
@@ -18,18 +18,12 @@ import urllib.parse
 import uuid
 from flask import send_from_directory
 
-import sys
-sys.path.insert(0, "Classifier")
-import SMART_3
-import get_highlight
+import smart3wrapper
 
 
 server = Flask(__name__)
 app = dash.Dash(__name__, server=server, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
-
-# Loading database into memory
-DB, index_super = SMART_3.load_db(db_folder="./Classifier")
 
 NAVBAR = dbc.Navbar(
     children=[
@@ -56,6 +50,7 @@ DASHBOARD = [
             html.Div(id='version', children="Version - 0.1"),
             html.Br(),
             dbc.Textarea(className="mb-3", id='query_text', placeholder="NMR Spectrum", rows="20"),
+            html.Hr(),
             dcc.Loading(
                 id="structure",
                 children=[html.Div([html.Div(id="loading-output-5")])],
@@ -100,36 +95,10 @@ def handle_query(query_text):
     from io import StringIO
     data = StringIO(query_text)
     nmr_data_df = pd.read_csv(data, sep=None)
-    nmr_mat = SMART_3._convert_data(nmr_data_df, 1)
 
-    print(nmr_mat, file=sys.stderr)
-
-    # Running prediction here
-    query_dict = {}
-    query_dict["input_1"] = nmr_mat.tolist()
-
-    # # Handling SUPERCLASS
-    pred_url = "http://smart3-tf-server:8501/v1/models/CHANNEL1:predict"
-    payload = json.dumps({"instances": [ query_dict ]})
-
-    headers = {"content-type": "application/json"}
-    json_response = requests.post(pred_url, data=payload, headers=headers)
-
-    prediction_dict = json.loads(json_response.text)['predictions'][0]
-
-    # TODO: Will update names when hyunwoo updates model
-    fingerprint_prediction = np.asarray(prediction_dict["dense"])
-    pred_MW = np.asarray(prediction_dict["dense_1"])
-    pred_class = np.asarray(prediction_dict["class"])
-    pred_gly = np.asarray(prediction_dict["glycoside"])
-
-    fingerprint_prediction, pred_MW, pred_class_index, pred_class_prob, pred_gly = SMART_3.refine_predict_nmr([fingerprint_prediction], [pred_MW], [pred_class], [pred_gly])
-
-    experimental_mw = None
-    top_candidates = 10
-
-    topK = None
-    topK = SMART_3.search_database(fingerprint_prediction, pred_MW, DB, mw=experimental_mw, top_candidates=top_candidates)
+    # Drawing image
+    output_nmr_image = os.path.join("output", str(uuid.uuid4()) + ".png")
+    topK = smart3wrapper.search_smart3(nmr_data_df, output_image=output_nmr_image)    
 
     table_fig = dash_table.DataTable(
         columns=[
@@ -148,17 +117,19 @@ def handle_query(query_text):
         page_size= 10,
     )
 
-
-    # Drawing image
-    output_nmr_image = os.path.join("output", str(uuid.uuid4()) + ".png")
-    SMART_3._draw_search_image(output_nmr_image, nmr_mat, pred_class_index, pred_class_prob, pred_gly, index_super=index_super)
-
     return [table_fig, [html.Img(src="/plot/{}".format(os.path.basename(output_nmr_image)), width="1200px")]]
 
 @server.route("/plot/<uuid_save>")
 def download(uuid_save):
     """Serve a file from the upload directory."""
     return send_from_directory("./output", uuid_save)
+
+@server.route('/api/smart3/search', methods=['POST', 'GET'])
+def apismart3search():
+    df = pd.DataFrame(json.loads(request.values["peaks"]))
+    topK = smart3wrapper.search_smart3(nmr_data_df)
+
+    return json.dumps(topK.to_dict(orient="records"))
 
 # This gets you the model metadata
 @server.route("/model/metadata")
---
+++ smart3wrapper.py
@@ -0,0 +1,48 @@
+import sys
+import json
+import requests
+import numpy as np
+import pandas as pd
+
+sys.path.insert(0, "Classifier")
+import SMART_3
+import get_highlight
+
+
+# Loading database into memory
+DB, index_super = SMART_3.load_db(db_folder="./Classifier")
+
+def search_smart3(nmr_data_df, output_image=None):
+    nmr_mat = SMART_3._convert_data(nmr_data_df, 1)
+
+    # Running prediction here
+    query_dict = {}
+    query_dict["input_1"] = nmr_mat.tolist()
+
+    # # Handling SUPERCLASS
+    pred_url = "http://smart3-tf-server:8501/v1/models/CHANNEL1:predict"
+    payload = json.dumps({"instances": [ query_dict ]})
+
+    headers = {"content-type": "application/json"}
+    json_response = requests.post(pred_url, data=payload, headers=headers)
+
+    prediction_dict = json.loads(json_response.text)['predictions'][0]
+
+    # TODO: Will update names when hyunwoo updates model
+    fingerprint_prediction = np.asarray(prediction_dict["dense"])
+    pred_MW = np.asarray(prediction_dict["dense_1"])
+    pred_class = np.asarray(prediction_dict["class"])
+    pred_gly = np.asarray(prediction_dict["glycoside"])
+
+    fingerprint_prediction, pred_MW, pred_class_index, pred_class_prob, pred_gly = SMART_3.refine_predict_nmr([fingerprint_prediction], [pred_MW], [pred_class], [pred_gly])
+
+    experimental_mw = None
+    top_candidates = 10
+
+    topK = None
+    topK = SMART_3.search_database(fingerprint_prediction, pred_MW, DB, mw=experimental_mw, top_candidates=top_candidates)
+
+    if output_image is not None:
+        SMART_3._draw_search_image(output_image, nmr_mat, pred_class_index, pred_class_prob, pred_gly, index_super=index_super)
+
+    return topK
